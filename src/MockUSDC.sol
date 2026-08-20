44125781e692c5167c6931fb5e8e4160b17b517e
Bryan Eziokwu <ba.eziokwu@gmail.com>
2026-08-20 21:35:26 +0100

    test: add mock token burn support for loss simulation

--- src/MockUSDC.sol
+++ src/MockUSDC.sol
@@ -14,4 +14,8 @@ contract MockUSDC is ERC20 {
     function mint(address to, uint256 amount) public {
         _mint(to, amount);
     }
+
+    function burn(address from, uint256 amount) public {
+        _burn(from, amount);
+    }
 }
